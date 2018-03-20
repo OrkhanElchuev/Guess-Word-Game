7ff7fa6cecce66009e422758bed9c893ac77852d
Orkhan Elchuev <orkhan.elchuev@gmail.com>
2018-03-20 14:15:06 +0300

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,10 @@
+// Import libraries
+#include <iostream>
+#include <string>
 
+// Functions declaration
+void PrintIntro();
+void PlayGame();
+void PrintGameSummary();
+std::string GetValidGuess();
+bool AskToPlayAgain();
