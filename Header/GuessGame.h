9dc9153b9f1a31d8c9116798ea0d943f18367430
Orkhan Elchuev <orkhan.elchuev@gmail.com>
2018-03-20 14:36:16 +0300

    Update GuessGame.h

--- Header/GuessGame.h
+++ Header/GuessGame.h
@@ -1,1 +1,42 @@
+#pragma once
 
+// Include library
+#include <string>
+
+// Helpful variables for checking the validity of input and getting hidden access
+enum EGuessStatus{
+  	Invalid_Status,
+	OK,
+	Wrong_Length,
+	NOT_Lowercase,
+	Not_Isogram,
+	Hidden_Access
+};
+
+// Class declaration
+class GameGuess{
+  public:
+    GameGuess(); // Constructor 
+    
+    // Functions definition
+    void Reset();
+    int GetMaxTries() const;
+	  int GetCurrentTry() const;
+	  int GetHiddenWordLength() const;
+	  bool IsGameWon() const;
+	  EGuessStatus CheckGuessValidity(std::string) const;
+	  GameGuess SubmitValidGuess(std::string);
+	  std::string MyHiddenWord;
+	  int order = 0;
+	  int letter = 0;
+  
+  private: 
+    //Variables definition
+    int MyCurrentTry;
+	  bool bGameIsWon;
+	  bool IsLowerCase(std::string) const;
+	  bool IsIsogram(std::string) const;
+};
+
+  
+    
