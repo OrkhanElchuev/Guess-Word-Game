6cac90cdb3fef9d58af80662d11f345504bd3498
Orkhan Elchuev <orkhan.elchuev@gmail.com>
2018-03-21 15:05:28 +0300

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -34,3 +34,55 @@ void PrintIntro() {
 	std::cout << " Write <<show the word>> to reveal the word"; // Cheating phrase to unhide the word
 	return;
 }
+
+// Some motivational phrases after each try
+void MotivationPhrase(){
+	int CurrentTry = Game.GetCurrentTry();
+	if (CurrentTry == 1 || CurrentTry == 6) {
+		std::cout << "Pretty cool!\n";
+	}
+	else if (CurrentTry == 2 || CurrentTry == 7) {
+		std::cout << "Good!\n";
+	}
+	else if (CurrentTry == 3 || CurrentTry == 8) {
+		std::cout << "Almost!\n";
+	}
+	else if (CurrentTry == 4 || CurrentTry == 9) {
+		std::cout << "Great!\n";
+	}
+	else if (CurrentTry == 5 || CurrentTry == 10) {
+		std::cout << "Incredible!\n";
+	}
+}
+
+// Get the guess from the user and print it out
+std::string GetValidGuess() {
+	std::string guess = " ";
+	EGuessStatus Status = EGuessStatus::Invalid_Status; // Set current status of input as invalid
+	do {
+		int CurrentTry = Game.GetCurrentTry(); // Get the number of current try 
+		std::cout << "Try (" << CurrentTry << " of " << Game.GetMaxTries() << ") Please, enter your guess: ";
+		std::getline(std::cin, guess); // Get an input from the user
+		if (guess == "show the word") { // Special password for revealing the hidden word
+			std::cout << Game.MyHiddenWord << std::endl;
+			break;
+		}
+		// Checking validity of answer and printing different error messages
+		Status = Game.CheckGuessValidity(guess);
+		switch (Status) {
+		case EGuessStatus::Not_Isogram: // Check for the repetition of letters
+			std::cout << "Please enter a word without repeating letters!\n\n";
+			break;
+		case EGuessStatus::Wrong_Length: // Check for the proper word length 
+			std::cout << "Please Enter a " << Game.GetHiddenWordLength() << " letter word!\n\n";
+			break;
+		case EGuessStatus::NOT_Lowercase: // Check if all the letters are lowercase
+			std::cout << "Please Enter all lowercase letters!\n\n";
+			break;
+		default: 
+			MotivationPhrase(); // Printing Motivational phrases
+			break;
+		}
+	} while (Status != EGuessStatus::OK);
+	return guess;
+}
